2f45b0cb84ba1663a5c59547bcc7e4ddf37bd9e9
Priyank <pseudo3@gmail.com>
2023-05-31 09:59:29 +0200

    Add project files.

---
+++ sqltest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "valheim.database.windows.net";
+                builder.UserID = "hugin";
+                builder.Password = "Greydwarf!";
+                builder.InitialCatalog = "BikeStores";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    String sql = "select brand_name from production.brands";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                
+                                Console.WriteLine("{0}", reader.GetString(0));
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
